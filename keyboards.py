200de6fd6d251b425587423e6623539cba36cf09
PanamBoor <67980104+PanamBoor@users.noreply.github.com>
2020-09-03 14:50:38 +0300

    Add files via upload

--- keyboards.py
+++ keyboards.py
@@ -10,6 +10,7 @@ istochniki_btn = KeyboardButton("6.Источники",callback_data = 'istochni
 kategoriy_btn = KeyboardButton("7.Категории",callback_data = 'kategoriy_btn')
 google_table_btn = KeyboardButton("8.Google-таблица",callback_data = 'google_table_btn')
 
+#подключение кнопок команды хелп
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(
     balance_btn, otchet_btn
 )
